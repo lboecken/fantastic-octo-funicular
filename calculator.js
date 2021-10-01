6cff176d9892e40848f93837729e877bd5e252da
lennart <lsboecken@gmail.com>
2021-10-01 15:19:06 -0400

    Finished Parser, Shutter-Yard Algorithm & PostfixSolver & Started HTML CSS.
    Added support for negative values.

--- calculator.js
+++ calculator.js
@@ -13,59 +13,91 @@ String.prototype.isNumber = function() {
     else {return false};
 }
 
+Array.prototype.isNumber = function() {
+    if (Number(this)) {return true}
+    else {return false};
+}
+
 function Parser(string) {
+    let String = '(' + string + ')';
     let infixExpression = [];
     let current_number = [];
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] != '+' && string[i] != '-' && string[i] != '*' && string[i] != '/' && string[i] != '(' && string[i] != ')' || string[i] == '.') {
-            current_number.push(string[i]);
+    for (let i = 0; i < String.length; i++) {
+        if (String[i] != '+' && String[i] != '-' && String[i] != '*' && String[i] != '/' && String[i] != '(' && String[i] != ')' || String[i] == '.') {
+            if (String[i] === 'N') {
+                current_number.push('-')
+            } else {       
+            current_number.push(String[i]);
+            }
         } else {
             if (current_number.length > 0) infixExpression.push(current_number.join(''));
             current_number = [];
-            infixExpression.push(string[i]);
+            infixExpression.push(String[i]);
         };
     } return infixExpression;
 }
-
 
 function ConvertInfixToPostfix(infix) {
     let postfixStack = [];
     let operandStack = [];
     let infixStack = infix;
-        while (infixStack.length > 0) {
-            if (infixStack[0].isNumber()) {
-                postfixStack.push(infixStack[0]);
+    while (infixStack.length > 0) {
+        if (infixStack[0].isNumber()) {
+            postfixStack.push(infixStack[0]);
+            infixStack.splice(0,1);
+        } else if (infixStack[0] != '(' && infixStack[0] != ')') {
+                if (PRECEDENCEVALUES[operandStack.at(-1)] >= PRECEDENCEVALUES[infixStack[0]]) postfixStack.push(operandStack.pop());
+                operandStack.push(infixStack[0]);
                 infixStack.splice(0,1);
-            } else if (infixStack[0] != '(' && infixStack[0] != ')') {
-                if (operandStack.length == 0) {
-                    operandStack.push(infixStack[0]);
-                    infixStack.splice(0,1);
-                } else {
-                    while (PRECEDENCEVALUES[infixStack[0]] <= PRECEDENCEVALUES[operandStack[0]]) {
-                        postfixStack.push(operandStack.pop());
-                        if (operandStack.length === 0);
-                        break;
-                    }
-                
-                    
-                }
-            } else {
-
+        } else if (infixStack[0] == '(') {
+            operandStack.push(infixStack[0]);
+            infixStack.splice(0,1);
+        } else if (infixStack[0] == ')') {
+            while (operandStack.at(-1) != '(') {
+                postfixStack.push(operandStack.pop());
             }
-        } while (operandStack.length > 0) {
-            postfixStack.push(operandStack.pop());
-        };
-        return [operandStack, postfixStack];
+            operandStack.pop();
+            infixStack.splice(0,1);
+        }
+    } while (operandStack.length > 0) {postfixStack.push(operandStack.pop())};
+return postfixStack;
+} 
+
+function PostfixSolver(postfixexpression) {
+    let PostfixStack = [];
+    while (postfixexpression.length > 0) {
+        if (postfixexpression[0].isNumber()) {
+            PostfixStack.push(postfixexpression[0]);
+            postfixexpression.splice(0,1);
+        } else {
+            const num1 = Number(PostfixStack.at(-2));
+            const num2 = Number(PostfixStack.at(-1));
+            PostfixStack.pop();
+            PostfixStack.pop();
+            const operand = postfixexpression.shift();
+            PostfixStack.push(ApplyOperand(num1, num2, operand));
+        }
+    } return PostfixStack;
 }
 
-// function Solver(math_expression) {}
+function ApplyOperand(num1, num2, operand) {
+    console.log[num1, num2, operand]
+    switch (operand) {
+        case '+': return num1 + num2;
+        case '-': return num1 - num2;
+        case '*': return num1 * num2;
+        case '/': return num1 / num2;
+        case '^': return num1 ^ num2;
+    }
+}
+
+
+
 test_case = '1+2+3+4'
 test_case2 = '11+2453+314+4'
 test_case3 = '.1+0.2+3.+4'
-test_case4 = '(4+5+6)*(4/4)'
+test_case4 = '((4+N5+6)*(4/N2)*5)'
 
-console.log(test4 = (Parser(test_case4)));
-// console.log(ConvertInfixToPostfix(test4));
-let test_array = [];
-console.log(test_array[0]);
-
+test4 = Parser(test_case4);
+console.log(test4_conv = ConvertInfixToPostfix(test4));
+console.log(answer = PostfixSolver(test4_conv));
